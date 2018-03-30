1a65d8ad32519531561615777950c90692930fbb
Emily Carlsen <emilytcyt@gmail.com>
2018-03-30 00:36:04 -0400

    Initia commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const ioHook = require('iohook');
+const express = require('express');
+const app = express();
+
+let startup = Date.now();
+let data = {};
+let timers = [];
+
+/* API Endpoint */
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', (req, res) => {
+  data.general = {
+    runtime: Date.now() - startup,
+  }
+  res.json(data)
+});
+app.listen(3000);
+
+/* Key Logger Ingest */
+ioHook.on("keydown", event => {
+  timers[event.keycode] = Date.now()
+});
+
+ioHook.on("keyup", event => {
+  if(data[event.keycode] === undefined) {
+    data[event.keycode] = {time: 0, duration: 0, count: 0, holds: 0, taps:0}
+  }
+
+  let delta = Date.now() - timers[event.keycode]
+
+  data[event.keycode].count += 1;
+  data[event.keycode].duration += delta;
+  data[event.keycode].time = Date.now()
+  data[event.keycode].holds += (delta > 240) ? 1 : 0
+  data[event.keycode].taps += (delta < 60) ? 1 : 0
+});
+
+ioHook.start();
---
+++ web_client/js/chart.js
@@ -0,0 +1,26 @@
+function drawChart(data) {
+  console.log(data)
+  Chart.defaults.global.legend.labels.usePointStyle = true;
+
+  var ctx = document.getElementById("myChart");
+  var myChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+          labels: data.map(value => value.key),
+          datasets: [{
+              label: 'Characater Count Occurance',
+              data: data.map(value => value.count),
+              backgroundColor: ["#084081", "#0868ac", "#2b8cbe", "#4eb3d3", "#7bccc4", "#a8ddb5", "#ccebc5", "#e0f3db", "#f7fcf0"]
+          }]
+      },
+      options: {
+          legend: {
+            position: 'right'
+          },
+          title: {
+            display: true,
+            text: 'Top 9 Keys by Count'
+          }
+      }
+  });
+}
